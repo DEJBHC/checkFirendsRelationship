ee0aee50fe226c1978958edb4965598392512513
DEJBHC <92491404+DEJBHC@users.noreply.github.com>
2023-12-26 23:26:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ d=u2.connect('设备名称')#连接设备名称
 p=get_friend_list()#获取好友列表
 def check_f():
     for pe in p:
-        d.click(0.827, 0.073)#点击搜索框
+        d(resourceId="com.tencent.mm:id/meb").click()#点击搜索框
         time.sleep(random.uniform(0.5,1.5))
         d.send_keys(pe)#输入昵称
         time.sleep(random.uniform(0.5,1.5))
@@ -30,6 +30,9 @@ def check_f():
         time.sleep(random.uniform(0.5,1.5))
         strs=d(resourceId="com.tencent.mm:id/lwo").get_text()#获取实名信息
         if not check_bricks(strs):#如果名字没有括号，则为异常好友（可能的原因：拉黑，删除，未实名，网络异常）
+            print(p[0])
+        #当然，我们是可以检测到异常好友弹窗的，如下代码。
+        #if d(resourceId="com.tencent.mm:id/jlh").exists:
             print(p[0])
         time.sleep(random.uniform(0.5,1.5))
         for i in range(5):#退回到主页
